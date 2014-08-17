ce835481dd17c9e44ff4920b7ea68116c84ec1ee
Raymond Cheng <ryscheng@cs.washington.edu>
2014-08-17 15:44:22 -0700

    Scala.js + Play template

---
+++ project/Build.scala
@@ -0,0 +1,104 @@
+import sbt._
+import Keys._
+import play.Play._
+import scala.scalajs.sbtplugin.ScalaJSPlugin._
+import ScalaJSKeys._
+import com.typesafe.sbt.packager.universal.UniversalKeys
+import com.typesafe.sbteclipse.core.EclipsePlugin.EclipseKeys
+
+object ApplicationBuild extends Build with UniversalKeys {
+
+  val scalajsOutputDir = Def.settingKey[File]("directory for javascript files output by scalajs")
+
+  override def rootProject = Some(scalajvm)
+
+  val sharedSrcDir = "shared"
+
+  lazy val scalajvm = Project(
+    id = "scalajvm",
+    base = file("jvm")
+  ) enablePlugins (play.PlayScala) settings (scalajvmSettings: _*) aggregate (scalajs)
+
+  lazy val scalajs = Project(
+    id   = "scalajs",
+    base = file("js")
+  ) settings (scalajsSettings: _*)
+
+  lazy val sharedScala = Project(
+    id = "sharedScala",
+    base = file(sharedSrcDir)
+  ) settings (sharedScalaSettings: _*)
+
+  lazy val scalajvmSettings =
+    Seq(
+      name := "play-project",
+      version := Versions.app,
+      scalaVersion := Versions.scala,
+      scalajsOutputDir := (crossTarget in Compile).value / "classes" / "public" / "javascripts",
+      compile in Compile <<= (compile in Compile) dependsOn (fastOptJS in (scalajs, Compile)),
+      dist <<= dist dependsOn (fullOptJS in (scalajs, Compile)),
+      libraryDependencies ++= Dependencies.scalajvm,
+      commands += preStartCommand,
+      EclipseKeys.skipParents in ThisBuild := false
+    ) ++ (
+      // ask scalajs project to put its outputs in scalajsOutputDir
+      Seq(packageExternalDepsJS, packageInternalDepsJS, packageExportedProductsJS, packageLauncher, fastOptJS, fullOptJS) map { packageJSKey =>
+        crossTarget in (scalajs, Compile, packageJSKey) := scalajsOutputDir.value
+      }
+    ) ++ sharedDirectorySettings
+
+  lazy val scalajsSettings =
+    scalaJSSettings ++ Seq(
+      name := "scalajs-project",
+      version := Versions.app,
+      scalaVersion := Versions.scala,
+      persistLauncher := true,
+      persistLauncher in Test := false,
+      libraryDependencies ++= Dependencies.scalajs
+    ) ++ sharedDirectorySettings
+
+  lazy val sharedScalaSettings =
+    Seq(
+      name := "shared-scala-project",
+      EclipseKeys.skipProject := true,
+      libraryDependencies ++= Dependencies.shared
+    )
+
+  lazy val sharedDirectorySettings = Seq(
+    unmanagedSourceDirectories in Compile += new File((file(".") / sharedSrcDir / "src" / "main" / "scala").getCanonicalPath),
+    unmanagedSourceDirectories in Test += new File((file(".") / sharedSrcDir / "src" / "test" / "scala").getCanonicalPath),
+    unmanagedResourceDirectories in Compile += file(".") / sharedSrcDir / "src" / "main" / "resources",
+    unmanagedResourceDirectories in Test += file(".") / sharedSrcDir / "src" / "test" / "resources"
+  )
+
+  // Use reflection to rename the 'start' command to 'play-start'
+  Option(play.Play.playStartCommand.getClass.getDeclaredField("name")) map { field =>
+    field.setAccessible(true)
+    field.set(playStartCommand, "play-start")
+  }
+
+  // The new 'start' command optimises the JS before calling the Play 'start' renamed 'play-start'
+  val preStartCommand = Command.args("start", "<port>") { (state: State, args: Seq[String]) =>
+    Project.runTask(fullOptJS in (scalajs, Compile), state)
+    state.copy(remainingCommands = ("play-start " + args.mkString(" ")) +: state.remainingCommands)
+  }
+}
+
+object Dependencies {
+  val shared = Seq()
+
+  val scalajvm = Seq(
+    "org.webjars" % "jquery" % "1.9.0"
+  ) ++ shared
+
+  val scalajs = Seq(
+    "org.scala-lang.modules.scalajs" %%% "scalajs-dom" % Versions.scalajsDom,
+    "org.scala-lang.modules.scalajs" %% "scalajs-jasmine-test-framework" % scalaJSVersion % "test"
+  ) ++ shared
+}
+
+object Versions {
+  val app = "0.1.0-SNAPSHOT"
+  val scala = "2.11.1"
+  val scalajsDom = "0.6"
+}
